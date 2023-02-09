2467293c28a21c5e74a32280f829d3f1216d4b58
developer980 <tudordin2002@gmail.com>
2023-02-09 21:51:41 +0200

    minor fixes

--- index.js
+++ index.js
@@ -170,6 +170,7 @@ app.post("/search_user", (req, res) => {
     }, () => {
         console.log(result[0])
         bcrypt.compare(password, result[0].password, (err, succes) => {
+            console.log("password matches")
             succes && res.send({
                 email: result[0].email,
                 username:result[0].username
