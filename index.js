43e9565b01c94a5a0abebb8ace57783331b784da
developer980 <tudordin2002@gmail.com>
2023-03-18 12:37:16 +0200

    minor fixes

--- index.js
+++ index.js
@@ -103,10 +103,7 @@ app.post("/confirm_user", (req, res) => {
     const result = []
     
     console.log("email: " + email)
-
-    // const response = db.collection("users").find({
-    //     email:email
-    // })
+    
     db.collection("users").findOne({
         email:email
     }, (err, user) => {
@@ -115,23 +112,6 @@ app.post("/confirm_user", (req, res) => {
         if (token == user.pass_token)
             user && res.send(user)
     })
-    // response.forEach(data => {
-    //     result.push({
-    //         email: data.email,
-    //         username: data.username,
-    //         password: data.password,
-    //         token:data.pass_token
-    //     })
-    // }, () => {
-    //     const data = result[0]
-    //     console.log(token + " = " + data.token)
-    //     if (token == data.token) {
-    //         res.send({
-    //             email: data.email,
-    //             username: data.username,
-    //         })
-    //     }
-//    })
 })
 
 app.post("/search_user", (req, res) => {
--- user_auth/search_User.js
+++ user_auth/search_User.js
@@ -6,42 +6,67 @@ module.exports = function Search_User(email, transport, password, db) {
     return new Promise((resolve, reject) => {
         const result = []
         try {
-            const response = db.collection("users").find({ email: email })
-            response.forEach(data => {
-                //if(response.email && response.password)
-                console.log(data)
-                result.push({
-                    email: data.email,
-                    password: data.password
-                })
-            }, () => {
-                console.log(result[0])
-                result[0].email && password ? bcrypt.compare(password, result[0].password, (err, succes) => {
-                    console.log("password matches")
-                    
-                    if (succes) {
-                        const token = generate_token(db, email)
-
-                        send_email(db, result[0].email, transport, "User confirmation",
-                            "Please confirm your user by accessing this ",
-                            `https://travel-website-with-mongodb-front-end-bszn.vercel.app/verify_${token}`)
-                            .then(result => resolve("Email sent"))
-                    }
-                    else {
-                        err && reject("error")
-                    }
-                    // succes ? resolve({
-                    //     email: result[0].email,
-                    //     username: result[0].username
-                    // }) : reject("error")
-                    // err && reject("error")
-                })
-                    :
-                reject('error')
+            
+            db.collection('users').findOne({
+                email: email
+            }, (err, user) => {
+                if (err) throw err
+                
+                if (user.email && password) {
+                    bcrypt.compare(password, user.password, (err, succes) => {
+                        console.log("password matches")
+                                
+                        if (succes) {
+                            const token = generate_token(db, email)
+            
+                            send_email(db, result[0].email, transport, "User confirmation",
+                                "Please confirm your user by accessing this ",
+                                `https://travel-website-with-mongodb-front-end-bszn.vercel.app/verify_${token}`)
+                                .then(result => resolve("Email sent"))
+                        }
+                        else {
+                            err && reject("error")
+                        }
+                    })
+                }
             })
         } catch (e) {
             // res.status(500).json({error:e.message})
             reject('error')
         }
+
+            // const response = db.collection("users").find({ email: email })
+            // response.forEach(data => {
+            //     //if(response.email && response.password)
+            //     console.log(data)
+            //     result.push({
+            //         email: data.email,
+            //         password: data.password
+            //     })
+            // }, () => {
+            //     console.log(result[0])
+            //     result[0].email && password ? bcrypt.compare(password, result[0].password, (err, succes) => {
+            //         console.log("password matches")
+                    
+            //         if (succes) {
+            //             const token = generate_token(db, email)
+
+            //             send_email(db, result[0].email, transport, "User confirmation",
+            //                 "Please confirm your user by accessing this ",
+            //                 `https://travel-website-with-mongodb-front-end-bszn.vercel.app/verify_${token}`)
+            //                 .then(result => resolve("Email sent"))
+            //         }
+            //         else {
+            //             err && reject("error")
+            //         }
+            //         // succes ? resolve({
+            //         //     email: result[0].email,
+            //         //     username: result[0].username
+            //         // }) : reject("error")
+            //         // err && reject("error")
+            //     })
+            //         :
+            //     reject('error')
+            // })
     })
 }
